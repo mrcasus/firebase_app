003731d2b7b83c7bebc90244c4308a3417228ce8
mrCasus <osmanaltuntas@hotmail.com>
2021-05-01 12:38:41 +0300

    Update main.dart
    fixed startup issue after forcing orientation

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
 
+
 void main() async {
-  SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   await Firebase.initializeApp();
   runApp(MyApp());
 }
